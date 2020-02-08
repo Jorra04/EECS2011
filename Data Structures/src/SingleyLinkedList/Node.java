f3522a0627eff0cf1965ba566c690894ffb242dc
jorra04 <jorra04@my.yorku.ca>
2020-02-08 18:42:07 -0500

    created Linked List class, added tester. Tester passed all tests. (Only simple testing has occurred).

---
+++ Data Structures/src/SingleyLinkedList/Node.java
@@ -0,0 +1,39 @@
+package SingleyLinkedList;
+
+public class Node <E>{
+	private E element;
+	private Node<E> next;
+	
+	
+	/*
+	 * Constructors
+	 */
+	public Node() {
+		this(null,null);
+	}
+	public Node(E e, Node<E> n) {
+		this.element = e;
+		this.next = n;
+	}
+	
+	
+	/*
+	 * Accessors
+	 */
+	public E getElement() {
+		return this.element;
+	}
+	public Node<E> getNext() {
+		return this.next;
+	}
+	
+	/*
+	 * Modifiers
+	 */
+	public void setElement(E newElem) {
+		this.element = newElem;
+	}
+	public void setNext(Node<E> next) {
+		this.next = next;
+	}
+}
