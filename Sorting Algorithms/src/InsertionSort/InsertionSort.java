f243c5c828c9c0644f40883506753d093b918d5e
jorra04 <jorra04@my.yorku.ca>
2020-02-16 16:27:38 -0500

    Added the body for insertion sort and selection sort.

--- Sorting Algorithms/src/InsertionSort/InsertionSort.java
+++ Sorting Algorithms/src/InsertionSort/InsertionSort.java
@@ -1,5 +1,35 @@
 package InsertionSort;
 
 public class InsertionSort {
-
+	public static void main(String[] args) {
+		int[] arr = {4,2,5,8,1,6,9};
+		System.out.println(printer(arr));
+		System.out.println("-----------");
+		insertionSort(arr, arr.length);
+		System.out.println(printer(arr));
+	}
+	public static void insertionSort(int[] arr, int n){
+			for(int i = 1; i < n; ++i) {
+				int key = arr[i];
+				int j = i -1;
+				while(j >=0 && arr[j] > key) {
+					arr[j+1] = arr[j];
+					j -- ;
+				}
+				arr[j+1] = key;
+			}
+	}
+	
+	public static String printer(int[] arr ) {
+		String str = "";
+		for(int i = 0; i < arr.length; i++) {
+			if(i != arr.length -1 ) {
+				str+= arr[i] + ", ";
+			}
+			else {
+				str+= arr[i];
+			}
+		}
+		return str;
+	}
 }
